3afdcb14eae0bac50baa0c010cd282dae0172d7a
ProfessorSam <Samderprofessor@gmail.com>
2022-02-05 00:13:01 +0100

    Removed admin permission for leaving #5

--- 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/commands/OneVsOneCommand.java
+++ 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/commands/OneVsOneCommand.java
@@ -1,5 +1,6 @@
 package com.github.gamedipoxx.oneVsOne.commands;
 
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,73 +19,92 @@ public class OneVsOneCommand implements CommandExecutor {
 			return false;
 		}
 		Player player = (Player) sender;
-		if (player.hasPermission("OneVsOne.admin")) {
-			if (args.length >= 1) {
-				switch (args[0]) {
-				case ("create"):
-					String message = "§cArena: " + Arena.createAndRegisterArena().getArenaName();
-					player.sendMessage(message);
-					OneVsOne.getPlugin().getLogger().info(message);
-					break;
-				case ("join"):
-					if (args.length == 2) {
-						if(OneVsOne.getArena().size() == 0) {
-							player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAAVAIBLE.getString());
-							break;
-						}
-						for (Arena arena : OneVsOne.getArena()) {
-							if(arena.getArenaName().equalsIgnoreCase(args[1])) {
-								arena.joinPlayer(player);
-								break;
-							}
-						}
-						player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
-					}
-					break;
-				case("delete"):
-					if(args.length != 2) {
-						break;
-					}
-					if(Arena.getArena(args[1]) == null) {
-						player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
-					}
-					else {
-						Arena.deleteAndUnregisterArena(Arena.getArena(args[1]));
-					}
-					break;
-				case("list"):
-					if(args.length != 1) {
-						break;
-					}
-					if(OneVsOne.getArena().size() == 0) {
-						player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
-					}
-					else {
-						for(Arena arena : OneVsOne.getArena()) {
-							player.sendMessage(Messages.PREFIX.getString() + arena.getArenaName());
-						}
-					}
-					break;
-				case("reload"):
-					if(args.length != 1) {
-						break;
-					}
-					OneVsOne.getPlugin().saveConfig();
-					OneVsOne.getPlugin().reloadConfig();
-					player.sendMessage(Messages.PREFIX.getString() + Messages.CONFIGRELOADED.getString());
-					
-					break;
-				case("leave"):
-					BungeeCordManager.connectPlayerToLobby(player);				
-					break;
-				default:
+		if (args.length >= 1) {
+			switch (args[0]) {
+			case ("create"):
+				if(!checkPermissions(player)) {
 					break;
 				}
-			} else {
-				player.sendMessage(Messages.PREFIX.getString() + Messages.COMMANDS.getString());
+				String message = "§cArena: " + Arena.createAndRegisterArena().getArenaName();
+				player.sendMessage(message);
+				OneVsOne.getPlugin().getLogger().info(message);
+				break;
+			case ("join"):
+				if(!checkPermissions(player)) {
+					break;
+				}
+				if (args.length == 2) {
+					if (OneVsOne.getArena().size() == 0) {
+						player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAAVAIBLE.getString());
+						break;
+					}
+					for (Arena arena : OneVsOne.getArena()) {
+						if (arena.getArenaName().equalsIgnoreCase(args[1])) {
+							arena.joinPlayer(player);
+							break;
+						}
+					}
+					player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
+				}
+				break;
+			case ("delete"):
+				if(!checkPermissions(player)) {
+					break;
+				}
+				if (args.length != 2) {
+					break;
+				}
+				if (Arena.getArena(args[1]) == null) {
+					player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
+				} else {
+					Arena.deleteAndUnregisterArena(Arena.getArena(args[1]));
+				}
+				break;
+			case ("list"):
+				if(!checkPermissions(player)) {
+					break;
+				}
+				if (args.length != 1) {
+					break;
+				}
+				if (OneVsOne.getArena().size() == 0) {
+					player.sendMessage(Messages.PREFIX.getString() + Messages.NOARENAFOUND.getString());
+				} else {
+					for (Arena arena : OneVsOne.getArena()) {
+						player.sendMessage(Messages.PREFIX.getString() + arena.getArenaName());
+					}
+				}
+				break;
+			case ("reload"):
+				if(!checkPermissions(player)) {
+					break;
+				}
+				if (args.length != 1) {
+					break;
+				}
+				OneVsOne.getPlugin().saveConfig();
+				OneVsOne.getPlugin().reloadConfig();
+				player.sendMessage(Messages.PREFIX.getString() + Messages.CONFIGRELOADED.getString());
+
+				break;
+			case ("leave"):
+				BungeeCordManager.connectPlayerToLobby(player);
+				break;
+			default:
+				break;
 			}
-		}
+		} 
 		return false;
-		
+
+	}
+	
+	private boolean checkPermissions(Player player) {
+		if(!player.hasPermission("OneVsOne.admin")) {
+			player.sendMessage(Messages.PREFIX.getString() + Messages.NOPERMISSION.getString());
+			player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
+			return false;
+		}
+		return true;
+				
 	}
 }
