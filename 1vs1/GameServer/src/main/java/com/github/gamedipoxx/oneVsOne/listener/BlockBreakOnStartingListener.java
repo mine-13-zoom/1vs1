bbb4a2c064085130b090fe3b89744a0443b2f296
ProfessorSam <Samderprofessor@gmail.com>
2022-03-02 22:09:39 +0100

    Fixed Falldamage while waiting

--- 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/listener/BlockBreakOnStartingListener.java
+++ 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/listener/BlockBreakOnStartingListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.github.gamedipoxx.oneVsOne.events.PlayerLeaveArenaEvent;
@@ -43,6 +44,21 @@ public class BlockBreakOnStartingListener implements Listener{
 			return;
 		}
 		if(!players.contains(event.getPlayer())) {
+			return;
+		}
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent event){
+		if(event.getEntity()== null) {
+			return;
+		}
+		if(event.getEntity() instanceof Player == false) {
+			return;
+		}
+		Player player = (Player) event.getEntity();
+		if(!players.contains(player)) {
 			return;
 		}
 		event.setCancelled(true);
