e156796cb1960d6f124ed307e2e227c81c6f9845
ProfessorSam <Samderprofessor@gmail.com>
2022-03-04 19:16:29 +0100

    Disable block dropping on tnt explosion

--- 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/listener/OnTntPlaceListener.java
+++ 1vs1/GameServer/src/main/java/com/github/gamedipoxx/oneVsOne/listener/OnTntPlaceListener.java
@@ -7,8 +7,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class OnTntPlaceListener implements Listener{
+	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerPlaceTntEvent(BlockPlaceEvent event) {
 		if(event.getBlock().getType() != Material.TNT) {
@@ -18,5 +20,13 @@ public class OnTntPlaceListener implements Listener{
 		event.getItemInHand().setAmount(event.getItemInHand().getAmount() - 1);
 		World world = event.getBlock().getLocation().getWorld();
 		world.spawnEntity(event.getBlock().getLocation(), EntityType.PRIMED_TNT);
+	}
+	
+	@EventHandler
+	private void onBlockBreakEvent(EntityExplodeEvent event) {
+		if(event.getEntityType() != EntityType.PRIMED_TNT) {
+			return;
+		}
+		event.setYield(0F);
 	}
 }
